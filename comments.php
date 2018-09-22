f858de85ae694fee60cb048b420eada8ab8992f7
Daniel Lewis <dan.lewis@enspiral.com>
2018-09-22 15:47:56 +0200

    update overides file

--- comments.php
+++ comments.php
@@ -17,74 +17,101 @@ if ( post_password_required() ) {
 }
 ?>
 
-<div id="comments" class="comments-area">
+?>
+<?php if ( sq_option( 'replace_wp_comments_with_fb_comments' ) == 1 ) { ?>
 
-	<?php if ( strpos(get_page_template_slug(), 'sobertools-single' )) : ?>
-	<?php comment_form(); ?>
+	<section class="container-wrap">
+		<div class="container">
+			<div id="comments" class="comments-area">
+				<div class="fb-comments" data-href="<?php echo get_permalink(); ?>" data-numposts="5" width="100%"></div>
+			</div><!-- #comments -->
+		</div>
+	</section>
+
+	<?php if ( sq_option( 'facebook_login' ) == 0 || is_user_logged_in() ) : ?>
+		<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+				var js, fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) return;
+				js = d.createElement(s); js.id = id;
+				js.src = "//connect.facebook.net/<?php echo apply_filters('kleo_facebook_js_locale', 'en_US'); ?>/sdk.js#xfbml=1&version=v2.8<?php if ( sq_option( 'fb_app_id' ) ) { echo '&appId=' . sq_option( 'fb_app_id' ); } ?>";
+				fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));
+		</script>
 	<?php endif; ?>
 
-  <?php kleo_comment_form(); ?>
 
-	<?php if ( have_comments() ) : ?>
+<?php } else { ?>
 
-	<h2 class="comments-title">
-		<?php
-			printf( _n( 'One comment', '%1$s comments', get_comments_number(), 'livingsober' ),
-				number_format_i18n( get_comments_number() ) );
-		?>
-	</h2>
+	<section class="container-wrap">
+		<div class="container">
 
-	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+      <?php if ( strpos(get_page_template_slug(), 'sobertools-single') ) : ?>
+        <?php kleo_comment_form(); ?>
+      <?php endif; ?>
 
-  	<?php if ( is_plugin_active( 'comment-popularity/comment-popularity.php' ) ) { ?>
-  	<ul class="sobertools-sort">
-  		<li <?php echo !isset($_GET['comments_sort']) ? 'class="selected"' : ''; ?>><a href="<?php the_permalink(); ?>#comments">Sort by most liked</a></li>
-  		<li <?php echo $_GET['comments_sort'] == 'recent' ? 'class="selected"' : ''; ?>><a href="<?php the_permalink(); ?>?comments_sort=recent#comments">Sort by most recent</a></li>
-  	</ul>
-  	<?php } ?>
+			<div id="comments" class="comments-area">
 
-  	<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-  		<h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'livingsober' ); ?></h1>
-  		<div class="nav-previous"><?php echo add_sorting_querystring(get_previous_comments_link( __( '&larr; Previous Comments', 'livingsober' ) ) ); ?></div>
-  		<div class="nav-next"><?php echo add_sorting_querystring(get_next_comments_link( __( 'Next Comments &rarr;', 'livingsober' ) ) ); ?></div>
-  	</nav><!-- #comment-nav-above -->
-	<?php endif; // Check for comment navigation. ?>
+				<div class="hr-title hr-long"><abbr><?php comments_number( esc_html__( '0 Comments', 'kleo_framework' ), esc_html__( '1 Comment', 'kleo_framework' ), __( '% Comments', 'kleo_framework' ) ); ?></abbr></div>
 
-  <?php
-    // Healthy Recipes
-    if ($post->ID === 1519) :
-  ?>
-  	<ul class="sobertools-sort">
-  		<li <?php echo !isset($_GET['comments_sort']) ? 'class="selected"' : ''; ?>><a href="<?php the_permalink(); ?>#comments">Sort by most liked</a></li>
-  		<li <?php echo $_GET['comments_sort'] == 'recent' ? 'class="selected"' : ''; ?>><a href="<?php the_permalink(); ?>?comments_sort=recent#comments">Sort by most recent</a></li>
-  	</ul>
-  <?php endif; ?>
+				<?php if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+					<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'kleo_framework' ); ?></p>
+				<?php endif; ?>
 
-	<ol class="comment-list">
-		<?php
-			wp_list_comments( array(
-				'style'      => 'ol',
-				'short_ping' => true,
-				'avatar_size'=> 42,
-				'callback' => 'custom_comment_callback'
-			) );
-		?>
-	</ol><!-- .comment-list -->
+				<?php if ( have_comments() ) : ?>
+					<div id="comments-list">
 
-	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
-	<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
-		<h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'livingsober' ); ?></h1>
-		<div class="nav-previous"><?php echo add_sorting_querystring(get_previous_comments_link( __( '&larr; Previous Comments', 'livingsober' ) ) ); ?></div>
-		<div class="nav-next"><?php echo add_sorting_querystring(get_next_comments_link( __( 'Next Comments &rarr;', 'livingsober' ) ) ); ?></div>
-	</nav><!-- #comment-nav-below -->
-	<?php endif; // Check for comment navigation. ?>
+            <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+            
+              <?php if ( is_plugin_active( 'comment-popularity/comment-popularity.php' ) ) { ?>
+                <ul class="sobertools-sort">
+                  <li <?php echo !isset($_GET['comments_sort']) ? 'class="selected"' : ''; ?>><a href="<?php the_permalink(); ?>#comments">Sort by most liked</a></li>
+                  <li <?php echo $_GET['comments_sort'] == 'recent' ? 'class="selected"' : ''; ?>><a href="<?php the_permalink(); ?>?comments_sort=recent#comments">Sort by most recent</a></li>
+                </ul>
+              <?php } ?>
 
-	<?php if ( ! comments_open() ) : ?>
-	<p class="no-comments"><?php _e( 'Comments are closed.', 'livingsober' ); ?></p>
-	<?php endif; ?>
 
-	<?php endif; // have_comments() ?>
-	<?php if ( strpos(get_page_template_slug(), 'sobertools-single' ) === false) : ?>
-	<?php comment_form(); ?>
-	<?php endif; ?>
-</div><!-- #comments -->
+							<div id="comments-nav-above" class="comments-navigation" role="navigation">
+								<div class="paginated-comments-links clearfix">
+									<?php
+									paginate_comments_links( array(
+										'type' => 'list',
+										'prev_text' => esc_html__( 'Previous', 'kleo_framework' ),
+										'next_text' => esc_html__( 'Next', 'kleo_framework' ),
+									));
+									?>
+								</div>
+							</div><!-- #comments-nav-above -->
+						<?php endif; // Check for comment navigation. ?>
+						<ol>
+							<?php wp_list_comments( 'type=all&callback=kleo_custom_comments' ); ?>
+						</ol>
+
+						<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+							<div id="comments-nav-below" class="comments-navigation" role="navigation">
+								<div class="paginated-comments-links comments-links-after clearfix">
+									<?php
+									paginate_comments_links( array(
+										'type' => 'list',
+										'prev_text' => esc_html__( 'Previous', 'kleo_framework' ),
+										'next_text' => esc_html__( 'Next', 'kleo_framework' ),
+									) );
+									?>
+								</div>
+							</div><!-- #comments-nav-below -->
+						<?php endif; // Check for comment navigation. ?>
+
+						<div class="activity-timeline"></div>
+					</div>
+
+				<?php endif; // have_comments() ?>
+
+				<?php if ( strpos(get_page_template_slug(), 'sobertools-single' ) === false) : ?>
+          <?php kleo_comment_form(); ?>
+        <?php endif; ?>
+
+			</div><!-- #comments -->
+		</div>
+	</section>
+
+<?php }
