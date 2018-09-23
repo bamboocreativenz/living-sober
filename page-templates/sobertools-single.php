f5177c35fa18e22eb19014ff006fd00a2936b0ae
Daniel Lewis <dan.lewis@enspiral.com>
2018-09-23 18:08:32 +0200

    switch back to comments

--- page-templates/sobertools-single.php
+++ page-templates/sobertools-single.php
@@ -28,7 +28,7 @@ get_header(); ?>
 					get_template_part( 'content', 'page' );
 					// If comments are open or we have at least one comment, load up the comment template.
           if ( comments_open() || get_comments_number() ) {
-            comments_template(get_stylesheet_directory() . '/alternative_comments.php', true);
+            comments_template(get_stylesheet_directory() . '/comments.php', true);
 					}
 				endwhile;
 			?>
