0903cb8946d188269cc78e03efc83189dc633042
Daniel Lewis <dan.lewis@rootsystems.nz>
2018-11-14 15:07:40 +1300

    test the oldie

--- page-templates/sobertools.php
+++ page-templates/sobertools.php
@@ -12,27 +12,32 @@ get_header(); ?>
 
 <?php get_template_part('page-parts/general-before-wrap'); ?>
 
-<?php if ( have_posts() ) : ?>
-	<?php
-		// Reorder comments
-		add_filter( 'comments_array', 'array_reverse' );
-		// Start the Loop.
-		while ( have_posts() ) : the_post();
+<div id="main-content" class="main-content">
 
-		get_template_part( 'content', 'page' );
-  ?>
+	<div id="primary">
+		<div class="container" role="main">
+			<div class="row">
+			<section class="col-sm-12 col-md-12 col-lg-12 contentPage detail">
+		
+      <?php
+        // Reorder comments
+        add_filter( 'comments_array', 'array_reverse' );
+    		// Start the Loop.
+				while ( have_posts() ) : the_post();
+					// Include the page content template.
+					get_template_part( 'content', 'page' );
+					// If comments are open or we have at least one comment, load up the comment template.
+          if ( comments_open() || get_comments_number() ) {
+            comments_template();
+					}
+				endwhile;
+			?>
+			</section>
+			
+			</div><!-- /row -->
+		</div><!-- /container -->
+	</div><!-- #primary -->
+</div><!-- #main-content -->	
 
-			<?php get_template_part( 'page-parts/posts-social-share' ); ?>
-			<!-- Begin Comments -->
-			<?php
-				if ( comments_open() || get_comments_number() ) {
-					kleo_comment_form();
-			} ?>
-      <!-- End Comments -->
-	<?php endwhile; ?>
-
-<?php endif; ?>
-
-<?php get_template_part('page-parts/general-after-wrap'); ?>
-
-<?php get_footer(); ?>
+<?php
+get_footer();
