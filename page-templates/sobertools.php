6acc577d457b105c906bbaf1456a16f9930528ea
Daniel Lewis <dan.lewis@rootsystems.nz>
2018-11-14 15:06:09 +1300

    rework again

--- page-templates/sobertools.php
+++ page-templates/sobertools.php
@@ -13,31 +13,22 @@ get_header(); ?>
 <?php get_template_part('page-parts/general-before-wrap'); ?>
 
 <?php if ( have_posts() ) : ?>
-		<?php
-	// Reorder comments
-	add_filter( 'comments_array', 'array_reverse' );
-	// Start the Loop.
-	while ( have_posts() ) : the_post();
-    ?>
+	<?php
+		// Reorder comments
+		add_filter( 'comments_array', 'array_reverse' );
+		// Start the Loop.
+		while ( have_posts() ) : the_post();
 
-  	<?php
 		get_template_part( 'content', 'page' );
-        ?>
+  ?>
 
-        <?php get_template_part( 'page-parts/posts-social-share' ); ?>
-
-        <?php if ( sq_option( 'page_comments', 0 ) == 1 ): ?>
-
-            <!-- Begin Comments -->
-            <?php
-			if ( comments_open() || get_comments_number() ) {
-				comments_template( '', true );
+			<?php get_template_part( 'page-parts/posts-social-share' ); ?>
+			<!-- Begin Comments -->
+			<?php
+				if ( comments_open() || get_comments_number() ) {
+					kleo_comment_form();
 			} ?>
-            <!-- End Comments -->
-
-        <?php endif; ?>
-
-
+      <!-- End Comments -->
 	<?php endwhile; ?>
 
 <?php endif; ?>
