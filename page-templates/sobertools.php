193cff41eac40b1a71ceb459f3773076e90daa26
Daniel Lewis <dan.lewis@rootsystems.nz>
2018-11-14 14:57:50 +1300

    add section tag

--- page-templates/sobertools.php
+++ page-templates/sobertools.php
@@ -12,22 +12,22 @@ get_header(); ?>
 
 <?php get_template_part('page-parts/general-before-wrap'); ?>
 
-<?php
-	// Reorder comments
-	add_filter( 'comments_array', 'array_reverse' );
-	// Start the Loop.
-	while ( have_posts() ) : the_post();
-		// Include the page content template.
-		get_template_part( 'content', 'page' );
-		// If comments are open or we have at least one comment, load up the comment template.
-		if ( comments_open() || get_comments_number() ) {
-			kleo_comment_form();
-		}
-	endwhile;
-?>
+<section>
+	<?php
+		// Reorder comments
+		add_filter( 'comments_array', 'array_reverse' );
+		// Start the Loop.
+		while ( have_posts() ) : the_post();
+			// Include the page content template.
+			get_template_part( 'content', 'page' );
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) {
+				kleo_comment_form();
+			}
+		endwhile;
+	?>
 </section>
 
-<?php
-get_footer();
+<?php get_footer(); ?>
 
 
