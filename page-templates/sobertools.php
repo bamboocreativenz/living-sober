29a9eb78e73ed780c418d47aef68c7ec767dff1b
Daniel Lewis <dan.lewis@rootsystems.nz>
2018-11-14 14:37:41 +1300

    rename id to main as per the rest of the templates.

--- page-templates/sobertools.php
+++ page-templates/sobertools.php
@@ -17,32 +17,29 @@ kleo_switch_layout('sobertools');
 
 <?php get_template_part('page-parts/general-before-wrap'); ?>
 
-<div id="main-content" class="main-content">
-
-	<div id="primary">
-		<div class="container" role="main">
-			<div class="row">
-			<section class="col-sm-12 col-md-12 col-lg-12 contentPage detail">
+<div id="main">
+	<div class="container" role="main">
+		<div class="row">
+		<section class="col-sm-12 col-md-12 col-lg-12 contentPage detail">
+	
+		<?php
+			// Reorder comments
+			add_filter( 'comments_array', 'array_reverse' );
+			// Start the Loop.
+			while ( have_posts() ) : the_post();
+				// Include the page content template.
+				get_template_part( 'content', 'page' );
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) {
+					kleo_comment_form();
+				}
+			endwhile;
+		?>
+		</section>
 		
-      <?php
-        // Reorder comments
-        add_filter( 'comments_array', 'array_reverse' );
-    		// Start the Loop.
-				while ( have_posts() ) : the_post();
-					// Include the page content template.
-					get_template_part( 'content', 'page' );
-					// If comments are open or we have at least one comment, load up the comment template.
-          if ( comments_open() || get_comments_number() ) {
-            kleo_comment_form();
-					}
-				endwhile;
-			?>
-			</section>
-			
-			</div><!-- /row -->
-		</div><!-- /container -->
-	</div><!-- #primary -->
-</div><!-- #main-content -->	
+		</div><!-- /row -->
+	</div><!-- /container -->
+</div><!-- #main -->	
 
 <?php
 get_footer();
