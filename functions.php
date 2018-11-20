f00dd1ee70e262e35ce3317fdfe77ca22913d910
Daniel Lewis <dan.lewis@rootsystems.nz>
2018-11-21 07:14:29 +1300

    init function

--- functions.php
+++ functions.php
@@ -72,3 +72,74 @@ function comment_comparator($a, $b) {
  }
  return $compared;
 }
+
+// Overide the Archive for category line in Kleo
+
+if ( ! function_exists( 'kleo_title' ) ):
+	/**
+	 *  Return the Page title string
+	 */
+
+	function kleo_title() {
+		$output = "";
+		if ( is_tag() ) {
+			$output = __( 'Tag Archive for:', 'kleo_framework' ) . " " . single_tag_title( '', false );
+		} elseif ( is_tax() ) {
+			$term   = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
+			$output = $term->name;
+		} elseif ( is_category() ) {
+			$output = __( 'Archive for category:', 'kleo_framework' ) . " " . single_cat_title( '', false );
+		} elseif ( is_day() ) {
+			$output = __( 'Archive for date:', 'kleo_framework' ) . " " . get_the_time( 'F jS, Y' );
+		} elseif ( is_month() ) {
+			$output = __( 'Archive for month:', 'kleo_framework' ) . " " . get_the_time( 'F, Y' );
+		} elseif ( is_year() ) {
+			$output = __( 'Archive for year:', 'kleo_framework' ) . " " . get_the_time( 'Y' );
+		} elseif ( is_author() ) {
+			$curauth = ( get_query_var( 'author_name' ) ) ? get_user_by( 'slug', get_query_var( 'author_name' ) ) : get_userdata( get_query_var( 'author' ) );
+			$output  = __( 'Author Archive', 'kleo_framework' ) . " ";
+
+			if ( isset( $curauth->nickname ) ) {
+				$output .= __( 'for:', 'kleo_framework' ) . " " . $curauth->nickname;
+			}
+		} elseif ( is_archive() ) {
+			$output = post_type_archive_title( '', false );
+		} elseif ( is_search() ) {
+			global $wp_query;
+			if ( ! empty( $wp_query->found_posts ) ) {
+				if ( $wp_query->found_posts > 1 ) {
+					$output = $wp_query->found_posts . " " . __( 'search results for:', 'kleo_framework' ) . " " . esc_attr( get_search_query() );
+				} else {
+					$output = $wp_query->found_posts . " " . __( 'search result for:', 'kleo_framework' ) . " " . esc_attr( get_search_query() );
+				}
+			} else {
+				if ( ! empty( $_GET['s'] ) ) {
+					$output = __( 'Search results for:', 'kleo_framework' ) . " " . esc_attr( get_search_query() );
+				} else {
+					$output = __( 'To search the site please enter a valid term', 'kleo_framework' );
+				}
+			}
+
+		} elseif ( is_front_page() && ! is_home() ) {
+			$output = get_the_title( get_option( 'page_on_front' ) );
+
+		} elseif ( is_home() ) {
+			if ( get_option( 'page_for_posts' ) ) {
+				$output = get_the_title( get_option( 'page_for_posts' ) );
+			} else {
+				$output = __( 'Blog', 'kleo_framework' );
+			}
+
+		} elseif ( is_404() ) {
+			$output = __( 'Error 404 - Page not found', 'kleo_framework' );
+		} else {
+			$output = get_the_title();
+		}
+
+		if ( isset( $_GET['paged'] ) && ! empty( $_GET['paged'] ) ) {
+			$output .= " (" . __( 'Page', 'kleo_framework' ) . " " . $_GET['paged'] . ")";
+		}
+
+		return $output;
+	}
+endif;
