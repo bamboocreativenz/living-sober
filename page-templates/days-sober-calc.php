055381b8f73645a7cf6b882703acd92f0c947726
Daniel Lewis <dan.lewis@rootsystems.nz>
2018-11-23 17:36:27 +1300

    update shape

--- page-templates/days-sober-calc.php
+++ page-templates/days-sober-calc.php
@@ -16,13 +16,8 @@
     
     <div class="daysSober">
     	<form method="post" id="daysSoberForm" action="">
-    		<div id="daysSoberPre">
-    	    <h3 class="withIcon">Days sober?</h3>
-    	    <p>Want to see how long it’s been? Enter in the date of your last drink.</p>
-    	    <input type="text" name="daysSoberDatepicker" id="daysSoberDatepicker" value="" readonly="readonly" class="text" />
-    	  </div>
-        <div class="daysSoberAction">
-          <p class="halfCalc">Want to see how much money you've saved? Enter your average weekly booze spend.</p>
+        <div id="daysSoberPre" class="daysSoberAction">
+          <input type="text" name="daysSoberDatepicker" id="daysSoberDatepicker" value="" readonly="readonly" class="text" />
           <input type="text" name="daysSoberMoney" id="daysSoberMoney" value="" class="text" />
           <button class="btn btn-primary btn-red">Calculate</button>
           <div id="cancel" class="btn btn-secondary btn-red">Cancel</div>
