2ec5e05ec2f22be1732fe32874b95331e8cae11f
Daniel Lewis <dan.lewis@rootsystems.nz>
2018-11-23 18:31:32 +1300

    add styling

--- page-templates/days-sober-calc.php
+++ page-templates/days-sober-calc.php
@@ -1,28 +1,42 @@
-<!-- days sober -->
-<div id="daysSoberResult">
-	    <span class="result">0</span>
-</div>
+<style>
+  .resultWrapper {
+    display: flex;
+    justify-content: space-around;
+  }
+  
+</style>
 
-<!-- money saved -->
-<div id="moneyResult">
-  <span class="savings">$0.00</span>
-</div>
+<div class="calculator">
+  <div class="resultWrapper">
+    <!-- days sober -->
+    <div id="daysSoberResult">
+          <span class="result">0</span>
+    </div>
 
-<!-- form -->
-<div class="formWrapper">
-    <div id="recalcButton">
-        <a href="javascript:;" class="recalc">Recalculate</a>
+    <!-- money saved -->
+    <div id="moneyResult">
+      <span class="savings">$0.00</span>
     </div>
-    
-    <div class="daysSober">
-    	<form method="post" id="daysSoberForm" action="">
-        <div id="daysSoberPre" class="daysSoberAction">
-          <input type="text" name="daysSoberDatepicker" id="daysSoberDatepicker" value="" readonly="readonly" class="text" />
-          <input type="text" name="daysSoberMoney" id="daysSoberMoney" value="" class="text" />
-          <button class="btn btn-primary btn-red">Calculate</button>
-          <div id="cancel" class="btn btn-secondary btn-red">Cancel</div>
-        </div>
-    
-      </form>  
-    </div>
+  </div>
+
+  <!-- form -->
+  <div class="formWrapper">
+      <div id="recalcButton" style="text-align:center;">
+          <a href="javascript:;" target="_self" class="recalc fl-button" role="button">
+              <span class="fl-button-text">Recalculate</span>
+          </a>
+      </div>
+      
+      <div class="daysSober">
+        <form method="post" id="daysSoberForm" action="">
+          <div id="daysSoberPre" class="daysSoberAction">
+            <input type="text" name="daysSoberDatepicker" id="daysSoberDatepicker" value="" readonly="readonly" class="text" />
+            <input type="text" name="daysSoberMoney" id="daysSoberMoney" value="" class="text" />
+            <button class="btn btn-primary btn-red">Calculate</button>
+            <div id="cancel" class="btn btn-secondary btn-red">Cancel</div>
+          </div>
+      
+        </form>  
+      </div>
+  </div>
 </div>
