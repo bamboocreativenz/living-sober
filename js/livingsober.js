978b20626c03023e649f4ec3d269bbe070992ef7
Daniel Lewis <agentlewis@users.noreply.github.com>
2019-11-04 10:07:23 +1300

    Update livingsober.js

--- js/livingsober.js
+++ js/livingsober.js
@@ -1,53 +1,53 @@
+/**
+* Landing Page video function
+*/
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player;
+function onYouTubeIframeAPIReady() {
+player = new window.YT.Player('player', {
+  playerVars: {
+	'origin': 'https://livingsober.org.nz',
+	'autoplay': 0,
+	'loop': 1,
+	'controls': 0,
+	'showinfo': 0,
+	'modestbranding': 1
+ },
+  events: {
+	'onReady': onPlayerReady,
+	'onStateChange': onPlayerStateChange
+  }
+});
+}
+
+var p = document.getElementById ("player");
+$(p).hide();
+
+function onPlayerStateChange(event) {
+if(event.data === 0) {          
+  $("#overlay-container").fadeIn(800);
+  $("#player").fadeOut(400);
+  player.pauseVideo();
+}
+if (event.data == YT.PlayerState.ENDED) {
+  $('.start-video').fadeIn('normal');
+}
+}
+
+function onPlayerReady(event) {
+  console.log("Ready: ", event)
+  $(document).on('click', '.start-video', function () {
+	event.target.playVideo();
+	$("#player").fadeIn();
+	$("#overlay-container").fadeOut(1200);
+  });
+}
+
 jQuery(document).ready(function() {
   $ = jQuery;
-	
-  /**
-   * Landing Page video function
-   */
-	var tag = document.createElement('script');
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-  var player;
-  function onYouTubeIframeAPIReady() {
-    player = new window.YT.Player('player', {
-      playerVars: {
-        'origin': 'https://livingsober.org.nz',
-        'autoplay': 0,
-        'loop': 1,
-        'controls': 0,
-        'showinfo': 0,
-        'modestbranding': 1
-     },
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
-      }
-    });
-  }
-  
-  var p = document.getElementById ("player");
-  $(p).hide();
-  
-  function onPlayerStateChange(event) {
-    if(event.data === 0) {          
-      $("#overlay-container").fadeIn(800);
-      $("#player").fadeOut(400);
-      player.pauseVideo();
-    }
-    if (event.data == YT.PlayerState.ENDED) {
-      $('.start-video').fadeIn('normal');
-    }
-  }
-
- function onPlayerReady(event) {
-	  console.log("Ready: ", event)
-	  $(document).on('click', '.start-video', function () {
-		event.target.playVideo();
-		$("#player").fadeIn();
-		$("#overlay-container").fadeOut(1200);
-	  });
-  }
   
   // Do not allow spaces on the username field on the register page.
   jQuery.validator.addMethod("noSpace", function(value, element) { 
