726800cae1638eb6ed156ac0ced444bf99964c53
Daniel Lewis <dan.lewis@bamboocreative.nz>
2023-06-28 16:30:42 +0200

    comment out custom shit

--- functions.php
+++ functions.php
@@ -1498,55 +1498,44 @@ require_once( KLEO_LIB_DIR . '/dynamic-css.php' );
 /***************************************************
  * :: CUSTOM CODE BELOW
  ***************************************************/
-add_filter( 'bp_activity_maybe_load_mentions_scripts', 'buddydev_enable_mention_autosuggestions', 10, 2 );
+// add_filter( 'bp_activity_maybe_load_mentions_scripts', 'buddydev_enable_mention_autosuggestions', 10, 2 );
 
-function buddydev_enable_mention_autosuggestions( $load, $mentions_enabled ) {
+// function buddydev_enable_mention_autosuggestions( $load, $mentions_enabled ) {
 
-    if( ! $mentions_enabled ) {
-        return $load;//activity mention is  not enabled, so no need to bother
-    }
-    //modify this condition to suit yours
-    if( is_user_logged_in() && is_page_template( 'page-templates/sobertools.php' ) ) {
-        $load = true;
-    }
+//     if( ! $mentions_enabled ) {
+//         return $load;//activity mention is  not enabled, so no need to bother
+//     }
+//     //modify this condition to suit yours
+//     if( is_user_logged_in() && is_page_template( 'page-templates/sobertools.php' ) ) {
+//         $load = true;
+//     }
 
-    return $load;
-}
+//     return $load;
+// }
 
-// Remove DNS Prefetch will solve this issue https://stackoverflow.com/a/51918472
-remove_action( 'wp_head', 'wp_resource_hints', 2 );
+// // Remove DNS Prefetch will solve this issue https://stackoverflow.com/a/51918472
+// remove_action( 'wp_head', 'wp_resource_hints', 2 );
 
-/* DL: Tell buddy press to use WP mail smtp
- * This will mean that mail wont work in dev/staging unless wp mail is set up as well
- */
-add_filter( 'bp_email_use_wp_mail', '__return_true' );
+// /* DL: Tell buddy press to use WP mail smtp
+//  * This will mean that mail wont work in dev/staging unless wp mail is set up as well
+//  */
+// add_filter( 'bp_email_use_wp_mail', '__return_true' );
 
-// Change the profile link to point to buddypress
-// https://www.designwall.com/question/dwqa-author-link-redirect-to-buddypress-profile/
+// // Disable the "Activity > Following" subnav item on a user's profile page
+// // note we are running this modified version of the plugin. this code is actually redundant but its here as a reference for not doing what we wanted.
+// // see https://github.com/agentlewis/buddypress-followers
+// add_filter( 'bp_follow_show_activity_subnav', '__return_false' );
 
-add_filter('dwqa_get_author_link', 'dwqa_buddypress_profile_link', 10, 3);
-	function dwqa_buddypress_profile_link($url, $user_id, $user){
-		if(function_exists('bp_core_get_user_domain')){
-			return bp_core_get_user_domain($user_id);
-		}
-	return $url;
-}
+// // DL disable wp-embeds as people posting youtube links lag the site.
+// function my_deregister_scripts(){
+//   wp_deregister_script( 'wp-embed' );
+// }
+// add_action( 'wp_footer', 'my_deregister_scripts' );
 
-// Disable the "Activity > Following" subnav item on a user's profile page
-// note we are running this modified version of the plugin. this code is actually redundant but its here as a reference for not doing what we wanted.
-// see https://github.com/agentlewis/buddypress-followers
-add_filter( 'bp_follow_show_activity_subnav', '__return_false' );
+// // DL make posts longer in activity feed
+// function bpfr_custom_length( $excerpt_length) {
+// 	$excerpt_length = '2000'; // change value to your need
+// 	return $excerpt_length;
 
-// DL disable wp-embeds as people posting youtube links lag the site.
-function my_deregister_scripts(){
-  wp_deregister_script( 'wp-embed' );
-}
-add_action( 'wp_footer', 'my_deregister_scripts' );
-
-// DL make posts longer in activity feed
-function bpfr_custom_length( $excerpt_length) {
-	$excerpt_length = '2000'; // change value to your need
-	return $excerpt_length;
-
-}
-add_filter( 'bp_activity_excerpt_length', 'bpfr_custom_length', 10, 1);
+// }
+// add_filter( 'bp_activity_excerpt_length', 'bpfr_custom_length', 10, 1);
